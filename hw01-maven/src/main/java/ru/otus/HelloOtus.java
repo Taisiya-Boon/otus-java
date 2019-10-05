b4f381829ec4d29e9bfcfafd7274f0c244d4e84d
TaisiyaBoon <BoonT-AD@yandex.ru>
2019-10-05 22:42:44 +0300

    hw01 изпользован метод из maven репозитория guava, com.google.common.math.IntMath.pow который позволяет произвести операцию возведения в степень для целочисленных типов. Также добавлены pom-файлы.

---
+++ hw01-maven/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,11 @@
+package main.java.ru.otus;
+
+import com.google.common.math.IntMath;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        System.out.println("3^5=" + IntMath.pow(3, 5));
+    }
+
+}
