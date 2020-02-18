78f004e15a8805ceff04dbb667b0318f7d753fa8
TaisiyaBoon <BoonT-AD@yandex.ru>
2020-02-19 02:40:30 +0300

    hw11 добавление интерфейса для ячеек банкомата;      добавление enum для номиналов;      переработка некоторых методов класса ATM

--- hw11-solid/src/main/java/ru/otus/Cell.java
+++ hw11-solid/src/main/java/ru/otus/Cell.java
@@ -1,32 +1,9 @@
 package ru.otus;
 
-import lombok.Getter;
-import lombok.Setter;
+public interface Cell {
 
-@Getter
-public class Cell {
+    public int addBanknote();
 
-    private final int parBanknote;
-
-    @Setter
-    private int quantity;
-
-    Cell(int parBanknote){
-        this.parBanknote = parBanknote;
-        this.quantity = 0;
-    }
-
-    public int addBanknote() {
-        return quantity++;
-    }
-
-    public int takeBanknote(int quantityBanknote) {
-        if (quantityBanknote > quantity){
-            throw new IndexOutOfBoundsException("Нет столько банкнот");
-        } else {
-            quantity -= quantityBanknote;
-            return quantityBanknote * parBanknote;
-        }
-    }
+    public int takeBanknote(int quantityBanknote);
 
 }
